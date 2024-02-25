017ba8f06ad9603324343fe35e4be60cb206a94a
Christopher Porter <christopher.porter@version1.com>
2024-02-25 13:09:52 +0000

    update date azure yml file

---
+++ BookWooks.OrderApi/tests/BookWooks.OrderApi.UnitTests/Core/OrderAggregate/OrderConstructor.cs
@@ -0,0 +1,27 @@
+﻿
+
+using BookWooks.OrderApi.Core.OrderAggregate;
+using Xunit;
+
+namespace BookWooks.OrderApi.UnitTests.Core.OrderAggregate;
+public class OrderConstructor
+{
+  private readonly string _testName = "test name";
+  public static readonly DeliveryAddress DeliveryAddress = new("Test Street", "Test City", "Test Country", "Test Post Code");
+  public static readonly Order Order1 = new("UserIdNo", "Crispy", DeliveryAddress, "12345", "11111", "Christopher", OrderStatus.Pending);
+
+  private Order? _testOrder;
+
+  private Order CreateOrder()
+  {
+    return Order1;
+  }
+
+  [Fact]
+  public void InitializesName()
+  {
+    _testOrder = CreateOrder();
+
+    Assert.Equal(_testName, _testOrder.Status.Name);
+  }
+}
