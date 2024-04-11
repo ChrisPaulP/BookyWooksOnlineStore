2028cc76f6e2080ac20f94f366194febea726390
Christopher Porter <christopher.porter@version1.com>
2024-04-11 10:38:21 +0100

    updating integration test

--- BookyWooks.OrderApi.IntegrationTests/UnitTest1.cs
+++ BookyWooks.OrderApi.IntegrationTests/UnitTest1.cs
@@ -1,5 +1,7 @@
+using BookWooks.OrderApi.Core.OrderAggregate.Entities;
+using BookWooks.OrderApi.Core.OrderAggregate.ValueObjects;
 using BookWooks.OrderApi.UseCases.Create;
-
+using OrderItem = BookWooks.OrderApi.UseCases.Create.OrderItem;
 namespace BookyWooks.OrderApi.IntegrationTests;
 
 public class UnitTest1 : OrderApiBaseIntegrationTest
@@ -13,20 +15,26 @@ public class UnitTest1 : OrderApiBaseIntegrationTest
     public async Task SuccesfullyCreateNewOrder()
     {
         //await AddAsync(new Order("1", "Chris Porter", GetPreconfiguredDeliveryAddresses(), CardTypeEnum.MasterCard, "001", "1212121212", "Chris Porter", DateTime.Now.AddYears(1)));
+        // Create a new customer
+        var customer = Customer.Create("Customer Name", "Unique Email");
+        var product = Product.Create("Book 1", "Book URL", 9.99M);
+        var product2 = Product.Create("Book 2", "Book URL", 5.99M);
+        await AddAsync(customer); // Add the customer to the database
+        await AddAsync(product); // Add the customer to the database
 
         var orderItems = new List<OrderItem>()
                     {
-                        new OrderItem(Guid.NewGuid(), 9.99M, 1 ),
-                        new OrderItem(Guid.NewGuid(), 5.99M, 4 ),
+                        new OrderItem(product.Id, product.Price, 1 ),
+                        new OrderItem(product2.Id, product2.Price, 4 ),
                     };
-        var DeliveryAddress = new Address("Test Street", "Test City", "Test Country", "Test Post");
-        var PaymentDetails = new PaymentDetails("1234 5678 9012 3456", "Christopher", "12/23", "123", 1);
+        var deliveryAddress = new Address("Test Street", "Test City", "Test Country", "Test Post");
+        var paymentDetails = new PaymentDetails("1234 5678 9012 3456", "Christopher", "12/23", "123", 1);
         var command = new CreateOrderCommand
       (
-           orderItems,
-           Guid.NewGuid(),
-           DeliveryAddress,
-           PaymentDetails
+            OrderItems: orderItems,
+            CustomerId: customer.Id,
+            DeliveryAddress: deliveryAddress,
+            PaymentDetails: paymentDetails
        );
 
         var commandCreated = await SendAsync(command);
